e19ddf318b08fd1568c92b9cade817b0628b3d67
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-05-30 12:50:12 -0500

    functions

---
+++ cs50functions.py
@@ -0,0 +1,26 @@
+# the keyword def let's you define your own function or innovate your own function
+# def hello(to="world"): # def defines the function hello, while : let python know that everything under the indentention is under the function hello
+#     print("hello,", to)
+#
+# hello()
+# name = input("what's your name? ")
+# hello(name)
+
+
+
+# def main():
+#     name = input("what's your name? ")
+#     hello(name)
+#
+# def hello(to="world"):
+#     print("hello,", to)
+#
+# main()
+
+#scope refers to the context a variable exist in the context you defined it
+#return the function returns a value that was pass into it
+
+
+def main():
+    x = int(input("what's x? "))
+    print("x squared is", square(x))
