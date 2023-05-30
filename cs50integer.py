ab5d1cbcd236565a095a79cbbad64ddd4a107964
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-05-30 11:11:58 -0500

    float and rounds

--- cs50integer.py
+++ cs50integer.py
@@ -4,8 +4,20 @@
 # print(z)
 
 #nested functions
-x = int(input("what's x?"))
-y = int(input("what's y?"))
-print(x + y)
+# x = int(input("what's x?"))
+# y = int(input("what's y?"))
+# print(x + y)
 
-#float
+#float allow for decimal points
+
+# x = float(input("what's x?"))
+# y = float(input("what's y?"))
+# print(x + y)
+
+#round, rounds the number up to the nearest integer
+#round(number[, ndigits])
+
+x = float(input("what's x?"))
+y = float(input("what's y?"))
+z = round(x + y)
+print(f"{z:,}") # :, output a value separated by the , (command)
