97a72905157160f8bbf7c70a5e19329257c36c61
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-05-30 23:56:49 -0500

    conditional

--- CS50Conditionals.py
+++ CS50Conditionals.py
@@ -4,3 +4,34 @@
 # <= less than or equal to
 # == represents equality comparing the left and the right
 # != not equal to
+
+# if condition
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+#
+# if x < y:
+#     print("x is less than y") #line 12 should only be executed if line 11 is true. A Boolean situation here
+#
+# if x > y:
+#     print("x is greater than y")
+#
+# if x == y:
+#     print("x is equal to y")
+
+
+# elif (else if) condition
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+elif x == y:
+    print("x is equal to y")
+
+
+# else condition
+
