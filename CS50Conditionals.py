e588057472dcdc2c99edeaec9e8a3df255085342
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-05-31 00:17:13 -0500

    conditionals

--- CS50Conditionals.py
+++ CS50Conditionals.py
@@ -55,11 +55,25 @@
 #     print("x is not equal to y")
 # else:
 #     print("x is equal to y")
+#
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+#
+# if x != y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
 
-x = int(input("What's x? "))
-y = int(input("What's y? "))
 
-if x != y:
-    print("x is not equal to y")
+# and
+score = int(input("Score"))
+if score >= 90 and score <+ 100:
+    print("Grade: A")
+elif score >= 80 and score < 90:
+    print("Grade: B")
+elif score >= 70 and score <80:
+    print("Grade: C")
+elif score >= 60 and score < 70:
+    print("Grade: D")
 else:
-    print("x is equal to y")
+    print("Grade: F")
