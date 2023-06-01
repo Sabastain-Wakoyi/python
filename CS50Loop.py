7ea04408e7d96e595e55d309cb3921ef17b85a65
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-05-31 23:53:28 -0500

    loop

--- CS50Loop.py
+++ CS50Loop.py
@@ -1,5 +1,10 @@
-#loops programming to do something in again and again
+#loop is a block of code doing something again and again
 print("meow")
 print("meow")
 print("meow")
 print("meow")
+
+#while is one keyword that can help us to programme a loop
+i = 3
+while i != 0:
+    print("meow")
