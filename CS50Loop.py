e335347706d1f067516d2dc414b6b7a071d8c14d
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-01 00:09:37 -0500

    loops

--- CS50Loop.py
+++ CS50Loop.py
@@ -1,11 +1,25 @@
 #loop is a block of code doing something again and again
-print("meow")
-print("meow")
-print("meow")
-print("meow")
+# print("meow")
+# print("meow")
+# print("meow")
+# print("meow")
 
 #while is one keyword that can help us to programme a loop
-i = 3
-while i != 0:
+# i = 3
+# while i != 0:
+#     print("meow")
+#     i = i - 1
+
+i = 0
+while i < 3:
     print("meow")
-    i = i - 1
+    # i = i + 1
+    i += 1
+
+
+#for loop
+#list is a data type in python containing a list of things
+
+for i in [0, 1, 2]: #[] (sqaure braket represents a list of itemss)
+    print("meow")
+
