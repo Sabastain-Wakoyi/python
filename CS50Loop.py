36055c29591baa7946b547fcf52b48f2c908187b
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-01 14:45:16 -0500

    list

--- CS50Loop.py
+++ CS50Loop.py
@@ -64,6 +64,9 @@
 
 students = ["Her", "Har", "Ron"]
 
-print(students[0])
-print(students[1])
-print(students[2])
+# print(students[0])
+# print(students[1])
+# print(students[2])
+
+for student in students:
+    print(student)
