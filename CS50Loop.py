fd7de5e47f249f5faf82bc43de21c42ccbfd05da
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-01 00:19:14 -0500

    loops

--- CS50Loop.py
+++ CS50Loop.py
@@ -10,16 +10,26 @@
 #     print("meow")
 #     i = i - 1
 
-i = 0
-while i < 3:
-    print("meow")
-    # i = i + 1
-    i += 1
+# i = 0
+# while i < 3:
+#     print("meow")
+#     # i = i + 1
+#     i += 1
 
 
 #for loop
 #list is a data type in python containing a list of things
 
-for i in [0, 1, 2]: #[] (sqaure braket represents a list of itemss)
+# for i in [0, 1, 2]: #[] (sqaure braket represents a list of itemss)
+#     print("meow")
+
+#range is a function in python
+for i in range(3):
     print("meow")
 
+
+# use _ (under score) when you don't care about a variable blc, it will not be used in the programme
+for _ in range(3):
+    print("meow")
+
+print("meow\n" * 3, end="") #end="" eliminates the extra line at the end of output
