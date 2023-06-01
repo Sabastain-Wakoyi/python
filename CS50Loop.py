dab6d08e0886f1f1a2035a63475bbc327a39bee3
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-01 11:05:34 -0500

    loops

--- CS50Loop.py
+++ CS50Loop.py
@@ -34,13 +34,29 @@
 
 # print("meow\n" * 3, end="") #end="" eliminates the extra line at the end of output
 
+#
+# while True:
+#     n = int(input("what's n?"))
+#     if n > 0:
+#         break
+#
+# for _ in range (n):
+#     print("meow")
 
-while True:
-    n = int(input("what's n?"))
-    if n > 0:
-        break
+def main():
+    number = get_number()
+    meow(number)
 
-for _ in range (n):
-    print("meow")
+def get_number():
+    while True:
+        n = int(input("what's n? "))
+        if n > 0:
+            break
+        return n
 
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+main()
 
