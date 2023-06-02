9eeeca884c6670ee8123541302f2cbc812276e2f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-02 17:16:30 -0500

    exception

--- CS50Exceptions.py
+++ CS50Exceptions.py
@@ -3,9 +3,11 @@
 
 try:
     x = int(input("What's x? "))
-    print(f"x is {x}")
-
 except ValueError:
     print("x is not an integer")
+else:
+    print(f"x is {x}")
 
 #ValueError, an due to no abiding to data type
+
+#NameError,
