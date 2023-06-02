38324bc7b10470edde6ef5b4dc0ecc187dc965bb
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-02 17:00:00 -0500

    exceptions

--- CS50Exceptions.py
+++ CS50Exceptions.py
@@ -1,7 +1,11 @@
 #SyntaxError must be solved by the developer
 #run time erros occur while your code is running
 
-x = int(input("What's x? "))
-print(f"x is {x}")
+try:
+    x = int(input("What's x? "))
+    print(f"x is {x}")
+
+except ValueError:
+    print("x is not an integer")
 
 #ValueError, an due to no abiding to data type
