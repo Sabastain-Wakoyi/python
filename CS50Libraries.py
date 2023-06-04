be2a112b92ffea8952c8979633d763ae61a77049
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-04 00:05:33 -0500

    library

--- CS50Libraries.py
+++ CS50Libraries.py
@@ -20,3 +20,12 @@ import random
 
 number = random.randint(1,10)
 print(number)
+
+#random.shuffle(x)
+
+import random
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
