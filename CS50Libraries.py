f7317b9659b38ac30ddb39450c551cf194a68472
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-04 00:07:41 -0500

    library

--- CS50Libraries.py
+++ CS50Libraries.py
@@ -6,6 +6,7 @@
 
 # import random
 
+#random.choice, allows you to make a choice between two things
 from random import choice # this is specific with the usage of the keyword from
 
 #coin = random.choice(["heads", "tails"])
@@ -14,14 +15,14 @@ print(coin)
 
 
 #random.randint
-#random.randint(a,b)
+#random.randint(a,b) takes 1 to 10
 
 import random
 
 number = random.randint(1,10)
 print(number)
 
-#random.shuffle(x)
+#random.shuffle(x) allows you get a list and shuffle it
 
 import random
 
