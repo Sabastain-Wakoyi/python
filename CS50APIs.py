3c3580c09bef1bc7e1070c1a289bd7b41f968579
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-07 22:40:26 -0500

    json

--- CS50APIs.py
+++ CS50APIs.py
@@ -16,6 +16,6 @@ if len(sys.argv) != 2 or not sys.argv[1]:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(response.json())
+print(json.dumps(response.json()))
 
 #docs.python.org/3/llibrary/json.html
