5a7bdc1d93864ebb189222b39b51a89ff2d4a932
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-08 12:48:53 -0500

    apis

--- CS50APIs.py
+++ CS50APIs.py
@@ -12,7 +12,7 @@ import json
 import requests
 import sys
 
-name = input("Enter music name")
+name = input("Enter music name: ")
 # if len(sys.argv) != 2 or not sys.argv[1]:
 if not name:
     sys.exit()
@@ -21,3 +21,13 @@ response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&ter
 print(json.dumps(response.json(), indent=2))
 
 #docs.python.org/3/llibrary/json.html
+import json
+import sys
+import requests
+
+if len(sys.argv) > 1:
+    response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+    # Rest of your code here
+    print(json.dumps(response.json(), indent=2))
+else:
+    print("Please provide a command-line argument.")
