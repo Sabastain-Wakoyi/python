2f68cb81fda0f2a0673b579bb33e202103370904
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-07 22:37:37 -0500

    apis

--- CS50APIs.py
+++ CS50APIs.py
@@ -1,9 +1,21 @@
+# import requests
+# import sys
+#
+#
+# if len(sys.argv) !=2:
+#     sys.exit()
+#
+# response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+# print(response.json())
+
+
 import requests
 import sys
 
-
-if len(sys.argv) !=2:
+if len(sys.argv) != 2 or not sys.argv[1]:
     sys.exit()
 
-
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(response.json())
+
+#docs.python.org/3/llibrary/json.html
