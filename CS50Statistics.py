dd97a5c5ebdd38f9c6664c165006c911d4ce5c5f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-07 21:36:36 -0500

    statistics

--- CS50Statistics.py
+++ CS50Statistics.py
@@ -1,1 +1,24 @@
-import statistics
+# import statistics
+#
+# print(statistics.mean([100, 90]))
+
+#command-line argument
+
+import sys
+
+#execution with the trying method
+
+# try:
+#     print("Hello, my name is", sys.argv[1])
+#
+# except IndexError:
+#     print("Too few arguments")
+
+
+#check for errors
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+#print name tags
+print("hello, my name is", sys.argv[1])
