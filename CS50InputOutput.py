17604aaa39044bd0ce9d98254dcf60237d8ffa9f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-06-30 16:09:02 -0500

    IO

--- CS50InputOutput.py
+++ CS50InputOutput.py
@@ -1,2 +1,11 @@
-name = input("what's your name? ")
-print(f"hello, {name}")
+names = []
+
+for _ in range(3):
+    #name = input("what's your name? ")
+    names.append(input("what's your name? "))
+
+for name in sorted(names):
+    print(f"hello, {name}")
+
+
+
