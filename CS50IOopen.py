133e8878ecab922d7c102b90616c088a91ff7c66
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-07-04 16:23:00 -0500

    I/O Open

--- CS50IOopen.py
+++ CS50IOopen.py
@@ -1,5 +1,6 @@
 name = input("what's your name? ")
 
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+#file = open("names.txt", "a")
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+#file.close()
