93eb8c5999fa0b655a2255549d8c0bbb1431bf6c
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-07-12 13:33:01 -0500

    sort

--- CS50CSV.py
+++ CS50CSV.py
@@ -1,4 +1,25 @@
+# with open("students.csv") as file:
+#     for line in file:
+#         row = line.rstrip().split(",")
+#         print(f"{row[0]} is in {row[1]}")
+#
+#
+
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         print(f"{name} is in {house}")
+#
+
+
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {name}")
+
+for students in sorted(students):
+    print(students)
+
+
