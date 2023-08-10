bba783385b0dcb25460062efe7b274ac4462f6c8
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-08-10 13:06:39 -0500

    read

--- CS50CSV.py
+++ CS50CSV.py
@@ -22,18 +22,54 @@
 # for students in sorted(students):
 #     print(students)
 
+#
+# students = []
+#
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+        # student = {}
+        # student["name"] = name
+        # student["house"] = house
+        #students.append(student)
 
+
+# def get_name(student):
+#     return student["name"]
+#
+#
+# for student in sorted(students, key=get_name, reverse=True):
+#     print(f"{student['name']} is in {student['house']}")
+
+
+
+# using lambda
+
+
+# students = []
+#
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
+#
+#
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is in {student['house']}")
+
+
+import csv
 
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        # student = {}
-        # student["name"] = name
-        # student["house"] = house
-        students.append(student)
+    reader = csv.reader(file)
+    for row in reader:
+        students.append({"name": row[0], "house": row[1]})
 
-for student in students:
+
+
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
