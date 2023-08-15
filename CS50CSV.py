92a868b4fba0640620c0fd5c5a907b67ff4ef172
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-08-15 14:01:42 -0500

    Dict writer

--- CS50CSV.py
+++ CS50CSV.py
@@ -80,5 +80,6 @@ house = input("what's your house? ")
 
 
 with open("students.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name, house])
+    # writer = csv.writer(file)
+    writer = csv.DictWriter(file, fieldnames=["name", "house"])
+    writer.writerow({"name": name, "house": house})
