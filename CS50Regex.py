5952627456f14c0c23efdb3c3b1331467f990072
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-08-23 18:01:03 -0500

    regexes

--- CS50Regex.py
+++ CS50Regex.py
@@ -1,11 +1,22 @@
 # Regexes stands for regular expressions.
 # Regular expressions are used in matching pattern
 
+#
+# email = input("what's your email").strip()
+#
+# if "@" in email and "." in email:
+#     print("valid")
+#
+# else:
+#     print("Invalid")
 
-email = input("what's your email").strip()
 
-if "@" in email and "." in email:
-    print("valid")
+email = input("what's your email? ").strip()
+
+username, domain = email.split("@")
+
+if username:
+    print("Valid")
 
 else:
     print("Invalid")
