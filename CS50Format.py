3d419d8b00807375f1fd66fc6ffe284122f88da6
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-11-10 13:37:52 -0600

    format

--- CS50Format.py
+++ CS50Format.py
@@ -1,2 +1,20 @@
-name = input("what's your name? ").strip()
+# import re
+#
+# name = input("what's your name? ").strip()
+# if "," in name:
+#     last, first = name.split(", ?")
+#     name = f"{first} {last}"
+# print(f"hello, {name}")
+#
+#
+#
+
+
+import re
+
+name = input("what's your name? "). strip()
+matches = re.search(r"^(.+), (.+$)", name)
+if matches:
+    name = matches.group(2) + " " + matches.group(2)
 print(f"hello, {name}")
+
