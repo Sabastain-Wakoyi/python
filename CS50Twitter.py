d9ab88317236e7a74ecc86e70592ad528e7857cc
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-11-10 16:01:35 -0600

    formating

--- CS50Twitter.py
+++ CS50Twitter.py
@@ -8,9 +8,8 @@ import re
 
 url = input("URL: ").strip()
 
-matches = re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
-if matches:
-    print(f"Username:", matches.group(1))
+if matches := re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(2))
 
 # username = re.sub(r"^https?://(www\.)?twitter\.com/", "", url)
 # print(f"Username: {username}")
