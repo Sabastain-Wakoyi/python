9ba6a9e3b08b967a10d63c3b2768340abbd2413e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-11-10 13:52:57 -0600

    format

--- CS50Format.py
+++ CS50Format.py
@@ -13,8 +13,7 @@
 import re
 
 name = input("what's your name? "). strip()
-matches = re.search(r"^(.+), *(.+$)", name)
-if matches:
+if matches := re.search(r"^(.+), *(.+$)", name):
     name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
 
